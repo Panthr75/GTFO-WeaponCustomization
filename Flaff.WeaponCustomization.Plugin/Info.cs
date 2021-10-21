e6860d2bd99b92233498fbe13dd8f363db9ccd88
Panthr75 <jjireland@stu.naperville203.org>
2021-10-21 11:06:00 -0500

    Setup

---
+++ Flaff.WeaponCustomization.Plugin/Info.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Flaff.WeaponCustomization.Plugin
+{
+    public static class Info
+    {
+        public const string GUID = "dev.flaff.gtfo.WeaponCustomization";
+        public const string NAME = "WeaponCustomization";
+        public const string VERSION = "0.1.0";
+    }
+}
---
+++ Flaff.WeaponCustomization.Plugin/WeaponCustomizationPlugin.cs
@@ -0,0 +1,15 @@
+﻿using BepInEx;
+using BepInEx.IL2CPP;
+using System;
+
+namespace Flaff.WeaponCustomization.Plugin
+{
+    [BepInPlugin(Info.GUID, Info.NAME, Info.VERSION)]
+    internal class WeaponCustomizationPlugin : BasePlugin
+    {
+        public override void Load()
+        {
+
+        }
+    }
+}
